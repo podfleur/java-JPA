7db27566627fd1367e147de5baea7589495adf2c
huyen <claire.ngn22@gmail.com>
2025-02-28 15:24:42 +0100

    TP2

--- src/main/java/fr/epsi/ConnexionJpa.java
+++ src/main/java/fr/epsi/ConnexionJpa.java
@@ -1,5 +1,6 @@
 package fr.epsi;
 
+import fr.epsi.entites.Livre;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -8,7 +9,13 @@ public class ConnexionJpa {
     public static void main(String[] args) {
         try (EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPA")) {
             EntityManager em = emf.createEntityManager();
-            System.out.println("Connexion à la BDD établie !");
+            em.getTransaction().begin();
+
+            Livre livre = new Livre(1, "La vie est bouche", "Dickens", "Romance", 1997);
+            System.out.println(livre);
+
+            em.persist(livre);
+            em.getTransaction().commit();
         }
     }
 }
---
+++ src/main/java/fr/epsi/entites/Livre.java
@@ -0,0 +1,84 @@
+package fr.epsi.entites;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "LIVRE")
+public class Livre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String titre;
+
+    @Column(nullable = false)
+    private String auteur;
+
+    @Column(nullable = false)
+    private String genre;
+
+    @Column(name = "ANNEE_PARUTION", nullable = false)
+    private int anneeParution;
+
+    public Livre() {}
+
+    public Livre(int id, String titre, String auteur, String genre, int anneeParution) {
+        this.id = id;
+        this.titre = titre;
+        this.auteur = auteur;
+        this.genre = genre;
+        this.anneeParution = anneeParution;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getAuteur() {
+        return auteur;
+    }
+
+    public void setAuteur(String auteur) {
+        this.auteur = auteur;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getAnneeParution() {
+        return anneeParution;
+    }
+
+    public void setAnneeParution(int anneeParution) {
+        this.anneeParution = anneeParution;
+    }
+
+    @Override
+    public String toString() {
+        return "Livre{" +
+                "id=" + id +
+                ", titre='" + titre + '\'' +
+                ", auteur='" + auteur + '\'' +
+                ", genre='" + genre + '\'' +
+                ", anneeParution=" + anneeParution +
+                '}';
+    }
+}
